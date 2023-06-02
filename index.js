a9b1ec4f30272792419341bcb75a77bd2e58fc5f
Israel <itepayotl@yahoo.com>
2023-06-02 15:22:57 -0700

    complete job

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (arrayDrivers) {
+    let drivers = [...arrayDrivers.slice(0,2)];
+    return drivers
+} 
+const returnLastTwoDrivers= function (arrayDriver) {
+    let drivers = [...arrayDriver.slice(2,4)];
+    return drivers
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+function createFareMultiplier(value) {
+    return function( ride = 5){
+    return ride * value;
+}
+}
+const fareDoubler = function(ride) {
+    return createFareMultiplier (2)(ride);
+}
+const fareTripler = function(ride) {
+    return createFareMultiplier(3)(ride)
+}
+function selectDifferentDrivers(driversArray, fn) {
+    return fn(driversArray)
+}
